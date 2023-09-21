d11a1f24b2f29e9c758ea1b4f96accc7045bcbbe
Jennifer Harper <jennifer@ambitious.co.nz>
2023-09-22 10:38:47 +1200

    slider

---
+++ web/js/slider.js
@@ -0,0 +1,37 @@
+
+
+const swiper1 = new Swiper(".swiper", {
+    // Optional parameters
+    direction: "horizontal",
+    loop: true,
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+    spaceBetween: 20,
+    centeredSlides: false,
+  
+    // autoplay: {
+    //   delay: 2500,
+    //   disableOnInteraction: false,
+    // },
+  
+    speed: 800,
+
+  
+    // If we need pagination
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: ".swiper-scrollbar",
+      draggable: true
+    }
+  });
