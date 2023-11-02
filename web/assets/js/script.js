6a682adb8b0c5f4f6fea17614bbcf595b1a4ef37
Jennifer Harper <jennifer@ambitious.co.nz>
2023-11-03 09:58:29 +1300

    updates

--- web/assets/js/script.js
+++ web/assets/js/script.js
@@ -4,16 +4,22 @@ $(window).resize(resizeTextBox);
 
 
 
-function resizeTextBox(){
-    var bigThing = $("#test").outerHeight();
-    var style3Text = $(".style-3 .intro-text").outerHeight();
-    var style3TextWidth = $(".style-3 .intro-text").width();
+function resizeTextBox() {
+  var bigThing = $("#test").outerHeight();
+  var style3Text = $(".style-3 .intro-text").outerHeight();
+  var style3TextWidth = $(".style-3 .intro-text").width();
+  var windowWidth = $(window).width();
 
-    if (bigThing < 300) {
-        $(".bg-img-one").css("height", "300px");
-    } else {
-      $(".bg-img-one").css("height", bigThing + "px");
-    }
+  if (bigThing < 300) {
+      $(".bg-img-one").css("height", "300px");
+  } else {
+      if (windowWidth > 2000) {
+          $(".bg-img-one").css("height", "20vh");
+      } else {
+          $(".bg-img-one").css("height", bigThing + "px");
+      }
+  }
+
 
     // residential and commercial pages hero image svg image
     if (style3TextWidth < 400) {
